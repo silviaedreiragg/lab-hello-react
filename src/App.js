a045f362f0942819c90bf8daf4dbb7ae78e4aeb7
silvia Edreira <silviaedreiragg@gmail.com>
2023-03-14 20:10:25 +0100

    lab done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Properties from './components/Properties';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar />
+      <div className="text-intro">
+        <h1>Say Hello to REACT JS</h1>
+        <p>You will learn how to use the most popular frontend library, ansd become a super ninja developer.</p>
+      </div>
+      
+      </div>
+      <Properties />
     </div>
   );
 }
